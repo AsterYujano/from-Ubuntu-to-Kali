9f273ac51eb4fa49b2a8925002f7fcc4faa845c7
Aster <32224751+AsterYujano@users.noreply.github.com>
2018-03-26 11:31:51 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 simple=('dsnmap')
-intermediate=.simple+('autre' 'etc...')
+#intermediate=.simple+('autre' 'etc...')
 
 if [ -z $1 ]
 then
   param='simple'
 fi
 
-for program in simple
+for i in ${simple[@]}
 do
-  sudo apt install $program
-  printf "[+] " $programe
+  sudo apt install $i
+  printf "[+] " $i
 done
 
 printf "[+] done" 
